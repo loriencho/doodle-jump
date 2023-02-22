a54e44f681688d72c8c5443b1dc7f250ad07793d
michele <michelebilko@gmail.com>
2023-02-22 18:19:02 -0500

    speedboost powerup created
    I THINK IT'S USING DECORATOR?

---
+++ DecoratorPattern/Assets/Scripts/SpeedBoost.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Powerups/SpeedBoost")]
+public class SpeedBoost : PowerupEffect
+{
+    public float amount;
+    public override void Apply(GameObject target)
+    {
+        target.GetComponent<PlayerController>().moveSpeed += amount;
+    }
+}
