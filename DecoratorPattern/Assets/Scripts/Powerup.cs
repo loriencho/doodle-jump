6ee62ba4782863adcf06a351ef9d9052d7de3e7b
michele <michelebilko@gmail.com>
2023-02-22 18:54:05 -0500

    powerup fix?

--- DecoratorPattern/Assets/Scripts/Powerup.cs
+++ DecoratorPattern/Assets/Scripts/Powerup.cs
@@ -7,7 +7,40 @@ public class Powerup : MonoBehaviour
     public PowerupEffect powerupEffect;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        Despawn();
         powerupEffect.Apply(collision.gameObject);
+    }
+
+    void Update()
+    {
+        if (CheckOutOfBounds())
+        {
+            Despawn();
+        }
+    }
+
+    public Vector3 PickPosition(float minimumHeight)
+    {
+        return new Vector3(
+            Random.Range(-7.7f, 7.7f),
+            GameManager.getCameraHeight() + minimumHeight,
+            0f
+        );
+    }
+
+    public bool CheckOutOfBounds()
+    {
+        return (GameManager.getCameraHeight() - 20f > transform.position.y);
+    }
+
+    public void Spawn(float minimumHeight)
+    {
+        gameObject.SetActive(true);
+        transform.position = PickPosition(minimumHeight);
+    }
+
+    public void Despawn()
+    {
+        gameObject.SetActive(false);
     }
 }
